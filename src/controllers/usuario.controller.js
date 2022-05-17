e8b8d8a2c1cc7e22ae9b599952964bec77fcef3f
BrandoAngulo <steven.sm.1992@gmai.com>
2022-05-17 17:46:27 -0500

    la misma parte

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -12,7 +12,7 @@ const getusuarios = async (req, res) => {
         res.send(error.message);
     }
 };
-
+//listar con id
 const getusuario = async(req, res) => {
     try {
         const {id_usuario} = req.params;
@@ -20,6 +20,18 @@ const getusuario = async(req, res) => {
         const result = await connection.query("SELECT id_usuario, nombre, cedula, departamento, ciudad, direccion, correo,  celular, referido, rol FROM usuario WHERE id_usuario = ? ",id_usuario);
         res.json(result);
         console.log(result);    
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+//DELETE
+const deleteUsuario = async(req, res) => {
+    try {
+        const {id_usuario} = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("DELETE FROM usuario WHERE id_usuario = ?",[usuario, id_usuario]);
     } catch (error) {
         res.status(500);
         res.send(error.message);
